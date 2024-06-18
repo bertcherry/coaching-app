cf7544d3b4485e9bf19651cd2f36ae897964b7b7
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-06-18 14:34:35 -0700

    add header

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import CoachingHeader from './components/CoachingHeader';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <CoachingHeader />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/CoachingHeader.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CoachingHeader() {
+    return (
+        <View style={{ flex: 0.2, backgroundColor: 'white' }}>
+            <Text style={{ padding: 40, fontSize: 30, color: 'd34f4f' }}>
+                Cherry Coaching Program
+            </Text>
+        </View>
+    );
+}
