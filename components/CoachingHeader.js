ee43c785effe73f8974bafd8f0e918f54a2865bb
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-06-18 14:45:34 -0700

    style head and foot

--- components/CoachingHeader.js
+++ components/CoachingHeader.js
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native';
 
 export default function CoachingHeader() {
     return (
-        <View style={{ flex: 0.16, backgroundColor: 'white' }}>
-            <Text style={{ padding: 40, fontSize: 26, color: 'd34f4f' }}>
+        <View style={{ flex: 0.16, backgroundColor: '#fba8a0' }}>
+            <Text style={{ padding: 40, fontSize: 26, fontWeight: 600, color: 'black' }}>
                 Cherry Coaching Program
             </Text>
         </View>
