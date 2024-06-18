c78e565b208e7e9c7c56fbf8dfe413f8f34fd0cc
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-06-18 14:41:28 -0700

    style header sizing

--- components/CoachingHeader.js
+++ components/CoachingHeader.js
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native';
 
 export default function CoachingHeader() {
     return (
-        <View style={{ flex: 0.2, backgroundColor: 'white' }}>
-            <Text style={{ padding: 40, fontSize: 30, color: 'd34f4f' }}>
+        <View style={{ flex: 0.16, backgroundColor: 'white' }}>
+            <Text style={{ padding: 40, fontSize: 26, color: 'd34f4f' }}>
                 Cherry Coaching Program
             </Text>
         </View>
