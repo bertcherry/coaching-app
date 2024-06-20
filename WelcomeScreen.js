fb2a1ac6605af1352f36bcb7e110603f9c335b8d
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-06-19 22:18:06 -0700

    style scrollable welcome

--- WelcomeScreen.js
+++ WelcomeScreen.js
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 export default function WelcomeScreen() {
     return (
-        <View  style={{ flex: 1 }}>
-            <Text style={{ padding: 40, fontSize: 24, fontWeight: 600, color: '#fae9e9', textAlign: 'center' }}>
+        <ScrollView style={{ flex: 1, padding: 20 }} indicatorStyle='white'>
+            <Text style={{ padding: 40, fontSize: 40, fontWeight: 600, color: '#fae9e9', textAlign: 'center' }}>
                 Welcome to your Cherry Coaching Program
             </Text>    
-            <Text style={{ padding: 20, fontSize: 20, color: '#fae9e9', textAlign: 'center' }}>
+            <Text style={{ padding: 20, fontSize: 30, color: '#fae9e9', textAlign: 'center', flexWrap: 'wrap' }}>
                 This is your portal to access your personalized coaching plan, record your workouts, and send and receive feedback with your coach.
             </Text>
-        </View>
+        </ScrollView>
     );
 }
