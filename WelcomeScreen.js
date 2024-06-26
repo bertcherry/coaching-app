0a7e83167f8d8f4efabcd9797801d41ea49a100d
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-06-26 11:53:09 -0700

    fix indicator prop and test

--- WelcomeScreen.js
+++ WelcomeScreen.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 
 export default function WelcomeScreen() {
     return (
-        <ScrollView style={{ flex: 1, padding: 20 }} indicatorStyle='white'>
-            <Text style={{ padding: 40, fontSize: 40, fontWeight: 600, color: '#fae9e9', textAlign: 'center' }}>
+        <ScrollView indicatorStyle={'white'} style={{ flex: 1}}>
+            <Text style={{ padding: 40, fontSize: 80, fontWeight: 600, color: '#fae9e9', textAlign: 'center' }}>
                 Welcome to your Cherry Coaching Program
             </Text>    
-            <Text style={{ padding: 20, fontSize: 30, color: '#fae9e9', textAlign: 'center', flexWrap: 'wrap' }}>
+            <Text style={{ padding: 20, fontSize: 50, color: '#fae9e9', textAlign: 'center', flexWrap: 'wrap' }}>
                 This is your portal to access your personalized coaching plan, record your workouts, and send and receive feedback with your coach.
             </Text>
         </ScrollView>
