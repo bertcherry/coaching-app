d9f1564bc7bbcda3516b8101cb7df9bb585520a0
bertcherry <119577715+bertcherry@users.noreply.github.com>
2024-07-18 15:01:35 -0700

    add login pressable

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,30 +1,36 @@
 import * as React from 'react';
-import { ScrollView, Text, StyleSheet, KeyboardAvoidingView, TextInput, Platform} from 'react-native';
+import { ScrollView, Text, StyleSheet, KeyboardAvoidingView, TextInput, Platform, Pressable} from 'react-native';
 
 export default function LoginScreen() {
     const [email, onChangeEmail] = React.useState('');
     const [password, onChangePassword] = React.useState('');
+    const [isLoggedIn, onLogin] = React.useState(false);
 
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.headerText}>Welcome to Cherry Coaching</Text>
-      <Text style={styles.regularText}>Login to continue </Text>
-      <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-        <TextInput 
-            value={email}
-            onChangeText={onChangeEmail}
-            placeholder='email'
-            keyboardType='email-address'
-            style={styles.input}
-        />
-        <TextInput 
-            value={password}
-            onChangeText={onChangePassword}
-            placeholder='password'
-            secureTextEntry={true}
-            style={styles.input}
-        />
-      </KeyboardAvoidingView>
+      <Text style={styles.regularText}>{isLoggedIn ? 'You are logged in' : 'Login to continue'}</Text>
+      {!isLoggedIn && (
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <TextInput 
+                value={email}
+                onChangeText={onChangeEmail}
+                placeholder='email'
+                keyboardType='email-address'
+                style={styles.input}
+            />
+            <TextInput 
+                value={password}
+                onChangeText={onChangePassword}
+                placeholder='password'
+                secureTextEntry={true}
+                style={styles.input}
+            />
+            <Pressable style={styles.button} onPress={() => {onLogin(!isLoggedIn)}}>
+                <Text style={styles.buttonText}>Login</Text>
+            </Pressable>
+        </KeyboardAvoidingView>
+      )}
     </ScrollView>
   );
 }
@@ -55,5 +61,17 @@ const styles = StyleSheet.create({
     fontSize: 16,
     borderColor: '#fba8a0',
     backgroundColor: '#fae9e9'
-},
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 8,
+    backgroundColor: '#fba8a0',
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 24,
+    color: 'black',
+    textAlign: 'center',
+  },
 });
