db5bcfc2c7a49f60f642fd088558d519bb0f3c37
bertcherry <119577715+bertcherry@users.noreply.github.com>
2025-06-18 11:24:53 -0700

    add reset password screen

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import SignInScreen from './screens/SignInScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import ConfirmEmailScreen from './screens/ConfirmEmailScreen';
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
+import ResetPasswordScreen from './screens/ResetPasswordScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import SampleWorkout from './screens/SampleWorkout';
 import CoachingHeader from './components/CoachingHeader';
@@ -26,6 +28,8 @@ export default function App() {
               <Drawer.Screen name='Sign In' component={SignInScreen} />
               <Drawer.Screen name='Sign Up' component={SignUpScreen} />
               <Drawer.Screen name='Confirm Email' component={ConfirmEmailScreen} />
+              <Drawer.Screen name='Forgot Password' component={ForgotPasswordScreen} />
+              <Drawer.Screen name='Reset Password' component={ResetPasswordScreen} />
               <Drawer.Screen name='Workout Preview' component={WorkoutPreview} 
                 // options={{drawerItemStyle: {display: 'none'}}} 
                 initialParams={{id: 'c8d08b56-1303-41d3-ae6f-8883f2f396b7'}} />
---
+++ screens/ResetPasswordScreen.js
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { ScrollView, Text, StyleSheet, KeyboardAvoidingView, TextInput, Platform } from 'react-native';
+import CustomButton from '../components/Button';
+
+export default function ResetPasswordScreen() {
+    const [code, onChangeCode] = React.useState('');
+    const [newPassword, onChangeNewPassword] = React.useState('');
+
+    const onSetPressed = () => {};
+    const onSignInPressed = () => {};
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.regularText}>Reset your password</Text>
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <TextInput 
+                value={code}
+                onChangeText={onChangeCode}
+                placeholder='code'
+                secureTextEntry={true}
+                style={styles.input}
+            />
+            <TextInput 
+                value={newPassword}
+                onChangeText={onChangeNewPassword}
+                placeholder='new password'
+                secureTextEntry={true}
+                style={styles.input}
+            />
+            <CustomButton onPress={onSetPressed} text="Set Password"></CustomButton>
+            <CustomButton onPress={onSignInPressed} text="Back to sign in" type="TERTIARY"></CustomButton>
+        </KeyboardAvoidingView>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  headerText: {
+    padding: 30,
+    fontSize: 30,
+    color: '#fae9e9',
+    textAlign: 'center',
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: '#fae9e9',
+    textAlign: 'center',
+  },
+  smallText: {
+    fontSize: 14,
+    padding: 8,
+    marginVertical: 8,
+    color: '#fae9e9',
+    textAlign: 'center',
+  },
+  link: {
+    color: '#fba8a0'
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: '#fba8a0',
+    backgroundColor: '#fae9e9'
+  },
+});
