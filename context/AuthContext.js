90039ee1185fcecc213564f23fecc1be6a049929
bertcherry <119577715+bertcherry@users.noreply.github.com>
2026-04-07 20:22:47 -0700

    docs remove logging

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -50,7 +50,6 @@ export function AuthProvider({ children }) {
     });
           
     if (!res.ok) {
-      console.log(res);
       const err = await res.json();
       throw new Error(err.error || 'Login failed');
     }
